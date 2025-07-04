8e884af73d7b69d2de5c7b6ddae0b37079fc4a74
Keerthiiit <keerthimrunalini05@gmail.com>
2025-07-04 13:58:14 +0530

    Update contestpage.jsx

--- frontend/src/pages/contestpage.jsx
+++ frontend/src/pages/contestpage.jsx
@@ -19,7 +19,7 @@ const ContestPage = () => {
     const fetchContests = async () => {
       try {
 
-        const response = await fetch('https://zcode-fullstack.onrender.com/contests');
+        const response = await fetch('https://zcode-fullstack-0m6q.onrender.com/contests');
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
