eff7d49b9a089444e49cdb8e6a614211851413e4
Keerthiiit <keerthimrunalini05@gmail.com>
2025-07-04 18:55:13 +0530

    Update footer.jsx

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 
   return (
     <div className="footer">
-      Created By Kameshwar &copy; {newdate.getFullYear()} 
+      Created By Keerthi Mrunalini &copy; {newdate.getFullYear()} 
       <br></br>
       <div className="followme">
 
@@ -17,14 +17,11 @@ const Footer = () => {
     
       <div className="follow-links">
 
-        <a href="https://www.linkedin.com/in/annigandla-kameshwara-rao/" target="_blank" title="LinkedIn" className="footer-icon">
+        <a href="https://www.linkedin.com/in/keerthi-m-8625b72b0/" target="_blank" title="LinkedIn" className="footer-icon">
           <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/linkedin.svg" alt="LinkedIn" width="22" className="footer-icon"></img>
         </a>
-        <a href="https://github.com/AnnigandlaKameshwaraRao" target="_blank" title="GitHub" className="footer-icon">
+        <a href="https://github.com/Keerthiiit" target="_blank" title="GitHub" className="footer-icon">
           <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/github.svg" alt="GitHub" width="22" className="footer-icon"></img>
-        </a>
-        <a href="https://www.instagram.com/kameshwara_rao_/" target="_blank" title="Instagram" className="footer-icon">
-          <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/instagram.svg" alt="Instagram" width="22" className="footer-icon"></img>
         </a>
       </div>
       </div>
