bfc9b5df2efa20bacf5150bdda6d6ecb92bd994b
Keerthiiit <keerthimrunalini05@gmail.com>
2025-07-04 13:51:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/zcode-fullstack/',  // 👈 important for subfolder deployment
   plugins: [react()],
 })
